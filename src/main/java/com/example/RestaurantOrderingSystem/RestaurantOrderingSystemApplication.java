8d2d9ca17cb03a488aac663447dfaf6237130edb
ThomasLee <mhleethomas@gmail.com>
2024-11-12 12:33:20 -0800

    Initialize Spring Boot project with Maven and essential dependencies

---
+++ src/main/java/com/example/RestaurantOrderingSystem/RestaurantOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantOrderingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestaurantOrderingSystem/RestaurantOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantOrderingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
