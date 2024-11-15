e0b064d965311e335e78fada01143be9b367cdf2
ThomasLee <mhleethomas@gmail.com>
2024-11-15 10:48:29 -0800

    Refactor HomeController and update CSS for improved link visibility

--- src/main/java/com/example/RestaurantOrderingSystem/HomeController.java
+++ src/main/java/com/example/RestaurantOrderingSystem/HomeController.java
@@ -3,10 +3,8 @@ package com.example.RestaurantOrderingSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -59,4 +57,5 @@ public class HomeController {
         model.addAttribute("menuItems", menuService.getMenuItems()); // Get menu items from MenuService
         return "viewMenu"; // Thymeleaf template for viewing menu
     }
+
 }
