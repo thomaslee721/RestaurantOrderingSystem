09711217fc0121448c0c1cb911d931a1dee02b35
ThomasLee <mhleethomas@gmail.com>
2024-11-18 11:13:45 -0800

    Rename main application class from RestaurantOrderingSystemApplication to Main

--- src/main/java/com/example/RestaurantOrderingSystem/Main.java
+++ src/main/java/com/example/RestaurantOrderingSystem/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantOrderingSystemApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantOrderingSystemApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
